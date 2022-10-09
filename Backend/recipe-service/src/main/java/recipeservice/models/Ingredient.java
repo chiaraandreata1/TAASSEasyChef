330295ffa680704758d520da22d438d7eb92c978
chiara.andreata <chiara1044.ca@gmail.com>
2022-10-09 18:10:22 +0200

    aggiunto ingredient ai models

---
+++ Backend/recipe-service/src/main/java/recipeservice/models/Ingredient.java
@@ -0,0 +1,36 @@
+package recipeservice.models;
+
+@JsonIgnoreProperties({"hybernateLazyInitializer","handler"})
+@Entity
+public class Ingredient {
+    @Id
+    @Column(name = "id_ingredient", nullable = false)
+    private Long idIngredient;
+    private String nameIngredient;
+    //private String quantityIngredient;
+
+    public String getNameIngredient() {
+        return nameIngredient;
+    }
+
+    public void setNameIngredient(String nameIngredient) {
+        this.nameIngredient = nameIngredient;
+    }
+
+    /*public String getQuantityIngredient() {
+        return quantityIngredient;
+    }
+
+    public void setQuantityIngredient(String quantityIngredient) {
+        this.quantityIngredient = quantityIngredient;
+    }*/
+
+    public Long getIdIngredient() {
+        return idIngredient;
+    }
+
+    public void setIdIngredient(Long idIngredient) {
+        this.idIngredient = idIngredient;
+    }
+
+}

bb3afd7a9dac2371cfb2280a6265837a6f7ce518
chiara.andreata <chiara1044.ca@gmail.com>
2022-10-09 20:20:04 +0200

    popolate classi per user service

--- Backend/user-service/src/main/java/userservice/controllers/UserController.java
+++ Backend/user-service/src/main/java/userservice/controllers/UserController.java
@@ -1,4 +1,41 @@
 package userservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import userservice.models.User;
+import userservice.repositories.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")   //si concatenerà api/v1/customers per restituire la lista di tutti gli utenti
+    public List<User> listAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("userbyid/{idUser}")// per ottenere l'ogetto utente passando l'id
+    public User userById(@PathVariable Long idUser){ return userRepository.findUserById(idUser);}
+
+    @PostMapping(value = "/user/create")// per aggiungere un nuovo utente
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@RequestBody User user){
+        userRepository.save(user);
+        System.out.println("New user created!");
+    }
+
+    // TODO - fare metodo per cambiare il nome dell'utente (OPZIONALE)
+
+    @DeleteMapping("/user/delete/{id}") // per eliminare un utente fornendo l'id
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id){
+        System.out.println("Delete user with id = " + id + "...");
+        userRepository.deleteById(id);
+        return new ResponseEntity<>("User deleted!", HttpStatus.OK);
+    }
 }
--- Backend/user-service/src/main/java/userservice/repositories/UserRepository.java
+++ Backend/user-service/src/main/java/userservice/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package userservice.repositories;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import userservice.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserById(Long idUser);
 }

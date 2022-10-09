059dd2366b843013e93a377f179ef407c227f849
chiara.andreata <chiara1044.ca@gmail.com>
2022-10-09 19:53:01 +0200

    modificato ingredient controller

--- Backend/recipe-service/src/main/java/recipeservice/controllers/IngredientController.java
+++ Backend/recipe-service/src/main/java/recipeservice/controllers/IngredientController.java
@@ -1,6 +1,10 @@
 package recipeservice.controllers;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import recipeservice.models.Ingredient;
 
 @RestController
 @RequestMapping("api/v1")

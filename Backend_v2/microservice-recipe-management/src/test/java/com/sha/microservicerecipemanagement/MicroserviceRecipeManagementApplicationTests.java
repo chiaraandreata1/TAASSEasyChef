e5c63b0bbabe98b3b121744fa8389551119a6383
Fortunto Cogliandro <jotun777@gmail.com>
2022-10-25 17:27:31 +0200

    aggiunta base per microservizio recipe

---
+++ Backend_v2/microservice-recipe-management/src/test/java/com/sha/microservicerecipemanagement/MicroserviceRecipeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.microservicerecipemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRecipeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a4bbbbeb6ed9b8230ee07ecf2d6b39574207d66d
chiara.andreata <chiara1044.ca@gmail.com>
2022-11-22 14:54:05 +0100

    Aggiunto servizio gateway

---
+++ Backend_v4/gatewayservice/src/main/java/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+import static org.springframework.boot.autoconfigure.SpringBootApplication.*;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        run(ApiGatewayApplication.class, args);
+    }
+}
